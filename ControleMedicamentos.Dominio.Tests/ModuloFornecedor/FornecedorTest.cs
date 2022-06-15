35b5cab061deb1cd2544c19477b8a49c50c5e93e
Tatiane Mossi <mossitatiane2@gmail.com>
2022-06-15 10:39:08 -0300

    Implementando testes unitários para a camada de domínio de Fornecedor.

--- ControleMedicamentos.Dominio.Tests/ModuloFornecedor/FornecedorTest.cs
+++ ControleMedicamentos.Dominio.Tests/ModuloFornecedor/FornecedorTest.cs
@@ -1,13 +1,95 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using ControleMedicamentos.Dominio.ModuloFornecedor;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Globalization;
 
 namespace ControleMedicamentos.Dominio.Tests.ModuloFornecedor
 {
     [TestClass]
     public class FornecedorTest
     {
+        public FornecedorTest()
+        {
+            CultureInfo.CurrentUICulture = new CultureInfo("pt-BR");
+        }
+
         [TestMethod]
-        public void Deve_inserir_um_fornecedor()
-        { 
+        public void Nome_do_fornecedor_deve_ser_obrigatorio()
+        {
+            //arrange
+            var fornecedor = new Fornecedor();
+
+            var validador = new ValidadorFornecedor();
+
+            //action
+            var resultado = validador.Validate(fornecedor);
+
+            //assert
+            Assert.AreEqual("'Nome' não pode ser nulo.", resultado.Errors[0].ErrorMessage);
+            Assert.AreEqual("'Nome' deve ser informado.", resultado.Errors[1].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Email_do_fornecedor_deve_ser_obrigatorio()
+        {
+            //arrange
+            var fornecedor = new Fornecedor();
+
+            var validador = new ValidadorFornecedor();
+
+            //action
+            var resultado = validador.Validate(fornecedor);
+
+            //assert
+            Assert.AreEqual("'Email' não pode ser nulo.", resultado.Errors[2].ErrorMessage);
+            Assert.AreEqual("'Email' deve ser informado.", resultado.Errors[3].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Telefone_do_fornecedor_deve_ser_obrigatorio()
+        {
+            //arrange
+            var fornecedor = new Fornecedor();
+
+            var validador = new ValidadorFornecedor();
+
+            //action
+            var resultado = validador.Validate(fornecedor);
+
+            //assert
+            Assert.AreEqual("'Telefone' não pode ser nulo.", resultado.Errors[4].ErrorMessage);
+            Assert.AreEqual("'Telefone' deve ser informado.", resultado.Errors[5].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Cidade_do_fornecedor_deve_ser_obrigatorio()
+        {
+            //arrange
+            var fornecedor = new Fornecedor();
+
+            var validador = new ValidadorFornecedor();
+
+            //action
+            var resultado = validador.Validate(fornecedor);
+
+            //assert
+            Assert.AreEqual("'Cidade' não pode ser nulo.", resultado.Errors[6].ErrorMessage);
+            Assert.AreEqual("'Cidade' deve ser informado.", resultado.Errors[7].ErrorMessage);
+        }
+
+        [TestMethod]
+        public void Estado_do_fornecedor_deve_ser_obrigatorio()
+        {
+            //arrange
+            var fornecedor = new Fornecedor();
+
+            var validador = new ValidadorFornecedor();
+
+            //action
+            var resultado = validador.Validate(fornecedor);
+
+            //assert
+            Assert.AreEqual("'Estado' não pode ser nulo.", resultado.Errors[8].ErrorMessage);
+            Assert.AreEqual("'Estado' deve ser informado.", resultado.Errors[9].ErrorMessage);
         }
     }
 }
